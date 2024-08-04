6aca5fc309afce9e672d967a873d6395c598093a
Shantanu2k19 <shantanusingh2030@gmail.com>
2024-08-05 00:23:33 +0530

    changes for doctor

--- app/urls.py
+++ app/urls.py
@@ -20,6 +20,9 @@ urlpatterns = [
     #for history
     path("get_history/",views.get_history,name="get_history"),
 
+    #for doc
+    path("doc_info/",views.doc_info,name="doc_info"),
+    path("get_verify_list/",views.get_verify_list,name="get_verify_list"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
