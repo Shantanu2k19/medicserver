b2643c572a55d7d0f33c670511c58337a96c102f
Shantanu2k19 <shantanusingh2030@gmail.com>
2024-08-05 22:44:50 +0530

    path fix

--- medicserver/settings.py
+++ medicserver/settings.py
@@ -108,7 +108,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://google-mymedic.vercel.app",
-    "https://mymedic.vercel.app/",
+    "https://mymedic.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True 
@@ -116,7 +116,7 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",  
     "https://google-mymedic.vercel.app",
-    "https://mymedic.vercel.app/",
+    "https://mymedic.vercel.app",
 ]
 
 CORS_ALLOW_HEADERS = [
